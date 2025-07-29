6eb59ea241521ae531e53543623edbf9d479111e
Sepehr Fallah <Sepehr.Fallah@gmail.com>
2025-07-29 05:11:56 -0700

    It now has a structure.

--- XO_Fakour.py
+++ XO_Fakour.py
@@ -1,4 +1,4 @@
-class game_page:
+class Board:
     
     
     def __init__(self):
@@ -13,39 +13,148 @@ class game_page:
             print()
             
             
-    def make_move(self, player, move):
+    def makes_move(self, player, move):
         
         if self.is_valid_move(move):
-            if player == f"{player.name}" :
-                self.map[move[0]][move[1]] = "X"
-            else:
-                self.map[move[0]][move[1]] = "O"
-            return self.map
-
-                
+            self.map[move[0]][move[1]] = player.symbol
+            
+        
         print("این خونه پره، یه خونه دیگه رو برای حرکت انتخاب کنی!")
         return self.map
 
     
     def is_valid_move(self, move):
-        return (self.map[move[0]][move[1]] == "_")
+        return (self.get_symbol(move) == "_")
             
     
+    def is_full(self):
+        for i in range(3):
+            for j in range(3):
+                if self.map[i][j] == "_":
+                    return False
+        return True
     
-class player:
+    def get_symbol(self, move):
+        return self.map[move[0]][move[1]]
+        
     
-    def __init__(self, name,):
+    
+class Player:
+    
+    def __init__(self, name, symbol):
         self.name = name
+        self.symbol = symbol
     
-    def player_move(self):
-        move = input(f"{self.player.name}  نوبتته، بازی کن")
+    def player_move(self, player):
+        move = input(f"{player.name}  نوبتته، بازی کن")
         try:
             move = int(move)
         except:
-            print(f"یچی بزن بفهمم چکار باید بکنم من {self.player.name}")
+            print(f"یچی بزن بفهمم چکار باید بکنم من {player.name}")
         else :
             return move
             
 
-a = game_page()
-a.show()
+
+
+class Game:
+    def __init__(self, board, player1 , player2 , current_player):
+        self.current_player = current_player
+        self.player1 = player1
+        self.player2 = player2        
+        self.board = board
+        
+    def switch_player(self):
+        """
+        bad az check_win call beshe 
+        """
+        if self.switch_player == self.player1:
+            self.switch_player =  self.player2
+        else:
+            self.switch_player =  self.player1
+            
+    def check_win(self, move):
+        """
+        bad az harekat dorost call beshe 
+        """
+        if self.board[move[0]][move[1]] == self.board[(move[0]+1)%3][move[1]] == self.board[(move[0]+2)%3][move[1]]:
+            return True
+        
+        if self.board[move[0]][move[1]] == self.board[move[0]][(move[1]+1)%3] == self.board[move[0]][(move[1]+1)%3]:
+            return True
+        if self.board[0][0] == self.board[1][1] == self.board[2][2] :
+            return True
+        if self.board[0][2] == self.board[1][1] == self.board[2][0] :
+            return True
+        return False
+    
+    
+    def play(self):
+        return
+        
+        
+class Bot:
+    def __init__(self , symbol):
+        if symbol.lower() == "x" :
+            self.symbol = "O"
+        else:
+            self.symbol = "X"
+
+        
+        
+        
+        
+        
+        
+        
+print()
+print("سلام سلام")
+name = input("برای شروع بازی، اسمتو وارد کن")
+symbol = input("دوست داری X باشی یا O؟")
+while True:
+    if symbol.lower() == "x" or symbol.lower() == "o" :
+        break
+    symbol = input("فقط X یا O رو برام وارد کن")
+player = Player(name, symbol.upper())
+bot = Bot(symbol)
+current_player = input("اگه میخوای اول شروغ کنی 1 و اگه میخوای دوم باشی 2 رو بفرست برام")
+while True:
+    try:
+        current_player = int(current_player)
+    except:
+        current_player=input("ترو قرآن یا 1 رو بفرست یا 2 رو")
+    else:
+        if current_player != 1 or current_player!= 2 :
+            current_player=input("ترو قرآن یا 1 رو بفرست یا 2 رو")
+        else:
+            if current_player == 1:
+                current_player = player
+            else:
+                current_player = bot
+            break
+                
+        
+board = Board()
+        
+        
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
